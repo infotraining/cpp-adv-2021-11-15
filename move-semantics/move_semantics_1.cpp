2c7e9232b85b576c0502661ac5593553459a46a4
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-11-15 12:33:19 +0100

    UniquePtr

--- move-semantics/move_semantics_1.cpp
+++ move-semantics/move_semantics_1.cpp
@@ -18,12 +18,48 @@ TEST_CASE("reference binding")
 
     SECTION("C++98")
     {
-        // TODO
+        std::string& ref_name = name;
+
+        const std::string& ref_full_name = full_name(name, "kowalski");
     }
 
     SECTION("C++11")
     {
-        // TODO
+        std::string&& ref_full_name = full_name(name, "kowalski");  
+        ref_full_name[0] = 'p';
+        std::cout << ref_full_name << "\n";
+
+        //std::string&& ref_name = name; // ERROR - cannot bind rvalue-ref to lvalue
     }
 }
 
+namespace Explain
+{
+    class vector
+    {
+    public:
+        void push_back(const std::string& item)
+        {
+            std::cout << item << " is copied to a vector\n";
+        }
+
+        void push_back(std::string&& item)
+        {
+            std::cout << item << " is moved to a vector\n";
+        }
+    };
+}
+
+TEST_CASE("using push_back")
+{
+    std::string name = "jan";
+
+    Explain::vector vec;
+
+    vec.push_back(name);
+    vec.push_back("adam");
+    vec.push_back(name + name);
+    vec.push_back(std::move(name));
+    
+    name = "ewa"; // OK - assignment after move is OK
+}
--- move-semantics/move_semantics_2.cpp
+++ move-semantics/move_semantics_2.cpp
@@ -10,19 +10,43 @@ class UniquePtr
 
 public:
     UniquePtr(nullptr_t)
-        : ptr_{nullptr}
+        : ptr_ {nullptr}
     {
     }
 
-    UniquePtr() : ptr_{nullptr}
-    {}
+    UniquePtr()
+        : ptr_ {nullptr}
+    {
+    }
 
     explicit UniquePtr(T* ptr)
-        : ptr_{ptr}
+        : ptr_ {ptr}
     {
     }
 
-    // TODO: implement move semantics
+    UniquePtr(const UniquePtr&) = delete;
+    UniquePtr& operator=(const UniquePtr&) = delete;
+
+    // move constructor
+    UniquePtr(UniquePtr&& source)
+        : ptr_ {source.ptr_}
+    {
+        source.ptr_ = nullptr;
+    }
+
+    // move assignment operator
+    UniquePtr& operator=(UniquePtr&& source)
+    {
+        if (this != &source)
+        {
+            delete ptr_; // deleting previous resource
+
+            ptr_ = source.ptr_;
+            source.ptr_ = nullptr;
+        }
+
+        return *this;
+    }
 
     ~UniquePtr()
     {
@@ -50,14 +74,51 @@ public:
     }
 };
 
-// TEST_CASE("2---")
-// {
-//     std::cout << "\n--------------------------\n\n";
-// }
+TEST_CASE("2---")
+{
+    std::cout << "\n--------------------------\n\n";
+}
 
-// TEST_CASE("move semantics - UniquePtr")
-// {
-//     // TODO
-//     UniquePtr<Gadget> pg1{new Gadget{1, "ipad"}};
-//     pg1->use();
-// }
+UniquePtr<Gadget> create_gadget()
+{
+    static int id_gen = 0;
+    const int id = ++id_gen;
+
+    UniquePtr<Gadget> ptr_g{new Gadget{id, "gadget-" + std::to_string(id)}};
+
+    return ptr_g;
+}
+
+void use(UniquePtr<Gadget> ptr_g)
+{
+    if (ptr_g)
+        ptr_g->use();
+}
+
+TEST_CASE("move semantics - UniquePtr")
+{
+    UniquePtr<Gadget> pg1 {new Gadget {1, "ipad"}};
+    pg1->use();
+
+    UniquePtr<Gadget> pg2 = std::move(pg1); // explicit
+    pg2->use();
+
+    UniquePtr<Gadget> pg3 {new Gadget {2, "smartwatch"}};
+    pg3->use();
+
+    pg3 = std::move(pg3); // explicit move
+
+    UniquePtr<Gadget> pg4 = UniquePtr<Gadget>(new Gadget{3, "smart-tv"}); // implicit move
+    pg4->use();
+
+    pg4 = UniquePtr<Gadget>{new Gadget{4, "roomba"}}; // implicit move
+    pg4->use();
+
+    UniquePtr<Gadget> pg5 = create_gadget();
+    pg5->use();
+
+    use(std::move(pg5)); // explicit move
+    use(create_gadget()); // implicit move
+
+    std::cout << "****\n";
+}
